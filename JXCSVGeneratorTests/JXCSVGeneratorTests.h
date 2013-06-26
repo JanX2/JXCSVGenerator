a04a3dcb203145cac4ff4243016b3cffaf4d6e45
Jan <github@geheimwerk.de>
2013-06-26 11:31:12 +0200

    Adding empty unit testing harness.

---
+++ JXCSVGeneratorTests/JXCSVGeneratorTests.h
@@ -0,0 +1,13 @@
+//
+//  JXCSVGeneratorTests.h
+//  JXCSVGeneratorTests
+//
+//  Created by Jan on 26.06.13.
+//  Copyright (c) 2013 Jan Weiß. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface JXCSVGeneratorTests : SenTestCase
+
+@end
---
+++ JXCSVGeneratorTests/JXCSVGeneratorTests.m
@@ -0,0 +1,35 @@
+//
+//  JXCSVGeneratorTests.m
+//  JXCSVGeneratorTests
+//
+//  Created by Jan on 26.06.13.
+//  Copyright (c) 2013 Jan Weiß. All rights reserved.
+//
+
+#import "JXCSVGeneratorTests.h"
+
+#import "JXCSVGenerator.h"
+
+
+@implementation JXCSVGeneratorTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+	STFail(@"Unit tests are not implemented yet in JXCSVGeneratorTests");
+}
+
+@end
